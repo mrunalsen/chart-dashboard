21a6874769a0697adeceb58d954ccd55dddaf84b
mrunalsen <mrunalpatel2500@gmail.com>
2023-05-20 18:34:46 +0530

    feat: bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Bootstrap Js import
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
